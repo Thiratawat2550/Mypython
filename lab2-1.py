1f34a04b81a8edd41d89ce663ff4262dc867c6ae
Thiratawat2550 <6529011009@cdti.ac.th>
2024-06-17 14:13:01 +0700

    first commit

---
+++ lab2-1.py
@@ -0,0 +1,5 @@
+x = int(input("คะแนน"))
+if x > 60:
+    print("ผ่าน")
+else: 
+    print("ไม่ผ่าน")
---
+++ lab2-2.py
@@ -0,0 +1,13 @@
+x = int(input("คะแนน"))
+if x >= 80:
+ print("เกรดA")
+elif x >= 70:
+ print("เกรดB")
+elif x >= 60:
+ print("เกรดC")
+elif x >= 50:
+ print("เกรดD")
+elif x >= 0:
+ print("เกรดF")
+else:
+ print("")
