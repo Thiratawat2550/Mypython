9a432c51f641e9495cbf4ed4664e89f0c1bf1080
Thiratawat2550 <6529011009@cdti.ac.th>
2024-06-24 14:48:29 +0700

    update

---
+++ lab4-2.py
@@ -0,0 +1,8 @@
+person = {"name": "thiratawat","age":18,"wt":66,"h":171}
+
+print(person["name"])
+print(person["age"])
+print(person["wt"])
+print(person["h"])
+person["name"] = "somsakul"
+print(person["name"])
